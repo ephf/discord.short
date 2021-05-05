14d4738e959ac893b557c007ae3688be227371c9
ephf <grant.wittmann@gmail.com>
2021-05-05 16:34:47 -0400

    color reset

--- anti-idle.js
+++ anti-idle.js
@@ -8,26 +8,26 @@ module.exports = function(id) {
             res.write('data');
         });
         req.on('end', function() {
-            console.log('\x1b[33m[discord.short.anti-idle] Server Success');
+            console.log('\x1b[33m[discord.short.anti-idle] Server Success\x1b[0m');
             res.end();
         });
     });
 
     server.listen(port, function(error) {
         if (error) {
-            console.log('\x1b[31m[discord.short.anti-idle] Something went wrong while running the dummy server:\n' + error);
+            console.log('\x1b[31m[discord.short.anti-idle] Something went wrong while running the dummy server:\n\x1b[0m' + error);
         } else {
-            console.log(`\x1b[33m[discord.short.anti-idle] Dummy server is listening on port https://localhost:${port}`);
+            console.log(`\x1b[33m[discord.short.anti-idle] Dummy server is listening on port https://localhost:${port}\x1b[0m`);
         }
     });
     
     setInterval(function() {
         rp(`https://${id.heroku.name}.herokuapp.com/`)
             .then(function(html) {
-                console.log('\x1b[33m[discord.short.anti-idle] Requesting Success');
+                console.log('\x1b[33m[discord.short.anti-idle] Requesting Success\x1b[0m');
             })
             .catch(function(err) {
-                console.log('\x1b[31m[discord.short.anti-idle] Requesting Error:\n'+ err);
+                console.log('\x1b[31m[discord.short.anti-idle] Requesting Error:\n\x1b[0m'+ err);
             });
     }, 20 * 60 * 1000);
 }
--- discordshort.js
+++ discordshort.js
@@ -5,11 +5,11 @@ let parse = require('./parser');
 let schemas = require('./schemas');
 
 function info() {
-    console.log('\x1b[36m[discord.short] ' + Array.from(arguments).join('\n[discord.short] '));
+    console.log('\x1b[36m[discord.short] ' + Array.from(arguments).join('\n[discord.short] ') + '\x1b[0m');
 }
 
 function error() {
-    let err = new Error('\x1b[31m[discord.short error] ' + Array.from(arguments).join('\n[discord.short error] ') + '\n');
+    let err = new Error('\x1b[31m[discord.short error] ' + Array.from(arguments).join('\n[discord.short error] ') + '\n\x1b[0m');
     throw err;
 }
 
