ef4ec38fa59749072cf31885dbbbfe854d1773fd
ephf <grant.wittmann@gmail.com>
2021-05-18 15:23:37 -0400

    yey

--- parser.js
+++ parser.js
@@ -53,7 +53,7 @@ module.exports = {
                                     label: name,
                                     args,
                                     permissions,
-                                    send(value) {return await m.channel.send(value)}
+                                    async send(value) {return await m.channel.send(value)}
                                 }
                                 ds.data.config = config;
                                 await command.failedPermissions(config);
