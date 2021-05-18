70dc4706a7978f6aefa318fef2dc3e927e392533
ephf <grant.wittmann@gmail.com>
2021-05-18 14:26:42 -0400

    fixed (again)

--- mongodb/mongo.js
+++ mongodb/mongo.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 module.exports = async function(id) {
-    await mongoose.connect(`mongodb+srv://${id.mongo.username}:${id.mongo.password}@${id.cluster}.mongodb.net/${id.mongo.database}?retryWrites=true&w=majority`, {
+    await mongoose.connect(`mongodb+srv://${id.mongo.username}:${id.mongo.password}@${id.mongo.cluster}.mongodb.net/${id.mongo.database}?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
